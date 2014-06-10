48a9aaf6340c453d82a57ad2abb9917a89258723
Matt Gale <matt@littleball.co.uk>
2014-06-10 13:38:12 +0100

    updating bundle

--- bin/js/bundle.js
+++ bin/js/bundle.js
@@ -280,7 +280,7 @@ module.exports = {
     }
 
     game.language = 'en';
-    game.stage.smoothed = false;
+    //game.stage.smoothed = false;
 
     game.state.start('preloader', true, false);
   }
